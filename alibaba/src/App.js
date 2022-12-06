03b2bb1437d7a0a653ff928aecb0108f81956dd1
soheil mohseni <mohsenisoheil61@gmail.com>
2022-12-07 01:01:50 +0330

    add flag

--- alibaba/src/App.js
+++ alibaba/src/App.js
@@ -30,8 +30,8 @@ function App() {
   // />
   const footer = (
     <>
-      <div 
-     // style={{position:"relative"}}
+      <div
+      // style={{position:"relative"}}
       >
         <Button label="Save" icon="pi pi-check" />
       </div>
@@ -43,29 +43,21 @@ function App() {
         <div class="flex flex-wrap justify-content-center card-container blue-container gap-6">
           {listofcu.map((cu, index) => {
             return (
-              // <div class="border-round w-12rem h-6rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center">2</div>
+              
+              <>
+              
+              <div style={{marginBottom: "36px" ,marginTop:"33px"}} class="border-round w-17rem h-74rem bg-white text-white font-bold flex align-items-center justify-content-start">
+              <div style={{display:"block",maxWidth:'100%' ,position: "relative" , bottom: '65px'}} >
+                <img style={{maxWidth:'272px', height: "14rem",}} src= {cu?.flags[1]}></img>                
+                <h2 style={{paddingLeft: "16px", color: 'black'}}> {cu?.name?.common}</h2>
+                <h5 style={{paddingLeft: "16px", color: 'black'}}> Population: {cu?.population}</h5>
+                <h5 style={{paddingLeft: "16px", color: 'black'}}> Region: {cu?.region}</h5>
+                <h5 style={{paddingLeft: "16px", color: 'black'}}> Capital: {cu?.capital}</h5>
 
-              <Card
-                key={index}
-                class="border-round w-25rem h-18rem bg-blue-500 text-white font-bold flex align-items-center justify-content-center"
-                title="Advanced Card" 
-                subTitle="Subtitle"
-                // style={{ width: "15em" ,}}
-                footer={footer}
-                header={<img
-                  alt="Card"
-                  src="images/usercard.png"
-                  style={{maxWidth: "100%",display:"block"}}
-                  onError={(e) =>
-                    (e.target.src = `${cu.flags[0]}`)
-                  }
-                />}
-              >
-                {/* <br/><br/><br/><br/> */}
-                <p className="m-0" style={{ lineHeight: "1.5" }}>
-                  a : bg b: 2 c:4
-                </p>
-              </Card>
+              </div>
+              
+              </div>
+              </>  
             );
           })}
         </div>
